3bfb510dfb63df0809175e00215bba9ae202b5f5
Bart Bucknill <bartbucknill@hotmail.com>
2017-08-02 16:31:21 +0100

    add script links in index.html, remove uneeded else

--- src/router.js
+++ src/router.js
@@ -1,60 +1,56 @@
-const http = require('http');
-const fs = require ('fs');
-const path = require ('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 //commented out getData because it triggers require of config.env which is not ye present thus crashing the app
 // const getData = require ('./queries/get_data');
 const whitelist = {
-  'index.html' : 'text/html',
-  'main.css' : 'text/css',
-  'favicon.ico' : 'image/x-icon',
-  'dom.js' : 'application/javascript',
-  'api_call.js' : 'application/javascript'
+  "index.html": "text/html",
+  "main.css": "text/css",
+  "favicon.ico": "image/x-icon",
+  "dom.js": "application/javascript",
+  "api_call.js": "application/javascript"
 };
 
 const router = (request, response) => {
-  let [nothing,endpoint,query] = request.url.split('/');
-          ///we'll use .split('/')[1] later if query not used
+  let [nothing, endpoint, query] = request.url.split("/");
+  ///we'll use .split('/')[1] later if query not used
 
-  if(endpoint === ''){
-    endpoint = 'index.html';
-  }
+  if (endpoint === "") {
+    endpoint = "index.html";
+  } 
+  else if (endpoint === "dbrequest") {
+    getData((err, dbResp) => {
+      //later getData will need to take input
+      if (err) return console.log(err);
+      let fromDb = JSON.stringify(dbResp);
 
-  else if (endpoint === 'dbrequest'){
-    getData ((err,dbResp) => {    //later getData will need to take input
-      if (err)
-        return (console.log(err));
-      let fromDb = JSON.stringify (dbResp);
-
-      response.writeHead (200, {'content-type' : 'application/json'});
+      response.writeHead(200, { "content-type": "application/json" });
       response.end(fromDb);
     });
-  }
-  else
-    if (whitelist[endpoint]) {
-      contentType = whitelist[endpoint];
-      if (contentType) {
-      const filePath = path.join(__dirname, '..', 'public', endpoint);
+  } 
+  if (whitelist[endpoint]) {
+    const contentType = whitelist[endpoint];
+    if (contentType) {
+      const filePath = path.join(__dirname, "..", "public", endpoint);
 
-    console.log ('looking for local file:', filePath);
+      console.log("looking for local file:", filePath);
 
       fs.readFile(filePath, (error, file) => {
         // but if there's error handle that first
         if (error) {
-          response.writeHead(500, {'Content-Type': 'text/html'});
+          response.writeHead(500, { "Content-Type": "text/html" });
           response.end("Sorry! We've had a problem!");
         } else {
           // otherwise provide the correct file
-          response.writeHead(200, {'Content-Type': contentType})
+          response.writeHead(200, { "Content-Type": contentType });
           response.end(file);
         }
       });
-    };
-
     }
-    else {
-      response.writeHead (404, {'content-type' : 'text/html'});
-      response.end('File not found. Gittoutta here!');
-    }
+  } else {
+    response.writeHead(404, { "content-type": "text/html" });
+    response.end("File not found. Gittoutta here!");
+  }
 };
 
-module.exports = router;   ///soo wrong!
+module.exports = router; ///soo wrong!
