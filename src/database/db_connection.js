e7df0ebf4ca76165bfaa0916e3f116d8945449bc
jen-harris <jennifer.louise.harris@gmail.com>
2017-08-02 15:11:51 +0100

    adding error check for user and password being an empty string relates #5

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -8,14 +8,14 @@ if(!process.env.DATABASE_URL){
 
 const params = url.parse(process.env.DATABASE_URL);
 
-const[username, password]= params.auth.split(':');
+const[user, password]= params.auth.split(':')||['',''];
 
 const options = {
   host: params.hostname;
   port: params.port;
   database: params.pathname.split('/')[1],
   max: process.env.DB_MAX_CONNECTIONS || 2,
-  user: username,
+  user,
   password
 }
 
