d39c1abbb3e821476924d681a291a1788dafdd85
jen-harris <jennifer.louise.harris@gmail.com>
2017-08-02 14:54:10 +0100

    created build.js, build.sql and connection.js relates #5

---
+++ src/database/db_build.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const buildDatabase = () => {
+  const connection = require('./db_connection');
+  const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+
+  connection.query(sql, (err, result) => {
+    if (err) {
+      // make more meaningful later - let the user know what's going on
+      console.log(err, 'error');
+    } else {
+      console.lod("database created");
+    }
+  });
+};
+
+builDatabase();
+
+module.exports = buildDatabase;
